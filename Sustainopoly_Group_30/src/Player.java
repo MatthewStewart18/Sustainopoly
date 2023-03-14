53dd44900fcb7ead32492e02a76834a6a817c601
40296786 <jyoung43@qub.ac.uk>
2023-03-14 00:29:28 +0000

    Replace Player.java

--- Sustainopoly_Group_30/src/Player.java
+++ Sustainopoly_Group_30/src/Player.java
@@ -9,6 +9,11 @@ public class Player {
 	private int playerMoney = 1500;
 	private final int playerID;
 
+	/**
+	 * constructor
+	 * @param name
+	 * @param playerID
+	 */
 	public Player(String name, int playerID) {
 		this.playerName = name;
 		this.playerID = playerID;
@@ -16,23 +21,42 @@ public class Player {
 	}
 
 
-
+	/**
+	 * method to return playerName
+	 * @return
+	 */
 	public String getName() {
 		return playerName;
 	}
-
+	
+	/**
+	 * method to return position
+	 * @return
+	 */
 	public int getPosition() {
 		return position;
 	}
 
+	/**
+	 * method to return money
+	 * @return
+	 */
 	public int getMoney() {
 		return playerMoney;
 	}
 
+	/**
+	 * method to return time
+	 * @return
+	 */
 	public int getTime() {
 		return playerTime;
 	}
 
+	/**
+	 * method to add money to a player's account
+	 * @param addMoney
+	 */
 	public void addMoney(int addMoney) {
 		if (playerMoney < -addMoney) {
 			noFunds(-addMoney - playerMoney);
@@ -40,22 +64,40 @@ public class Player {
 		this.playerMoney += addMoney;
 	}
 	
+	/**
+	 * method to add time to a player's account
+	 * @param addTime
+	 */
 	public void addTime(int addTime) {
 		if (playerMoney < -addTime) {
 			noFunds(-addTime - playerMoney);
 		}
 		this.playerTime += addTime;
 	}
-
+	
+	/**
+	 * method to print out 
+	 * @param amountNeeded
+	 */
 	private void noFunds(int amountNeeded) {
 		System.out.println("you are short $" + amountNeeded);
 	}
 
+	/**
+	 * method to pay
+	 * @param receiving
+	 * @param amount
+	 */
 	public void pay(Player receiving, int amount) {
 		receiving.addMoney(amount);
 		addMoney(-amount);
 	}
 
+	/**
+	 * method to move player
+	 * @param numSquares
+	 * @param board
+	 */
 	public void move(int numSquares, Board board) {
 		position += numSquares;
 
@@ -72,10 +114,19 @@ public class Player {
 		board.movePlayer(this.playerID, position);
 	}
 
+	/**
+	 * method to calculate where to move player and to call move
+	 * @param toPosition
+	 * @param board
+	 */
 	public void moveTo(int toPosition, Board board) {
 		move((25 - position + toPosition) % 25, board);
 	}
 
+	/**
+	 * method to invest
+	 * @param investment
+	 */
 	// adding invest to players investments
 	public void investIn(Investment investment) {
 		addMoney(-investment.getPrice());
