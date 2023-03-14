54c7fa70366c621ec61730558670301f6043c021
40296786 <jyoung43@qub.ac.uk>
2023-03-14 00:27:37 +0000

    Upload New File

---
+++ Sustainopoly_Group_30/test/CompletionBarTest.java
@@ -0,0 +1,36 @@
+import org.junit.jupiter.api.Test;
+
+import junit.framework.TestCase;
+
+public class CompletionBarTest extends TestCase {
+	
+//	protected void setUp() throws Exception {
+//		super.setUp();
+//		int completionBar=5,amount=7;
+//	}
+	
+
+	@Test
+	public void testGetValue() {
+		double expected = 5.0;
+		double actual = CompletionBar.getValue();
+		assertEquals(actual,expected);
+		System.out.println("Test for getValue() completed");
+	}
+
+	@Test
+	public void testSetValue() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testProgress() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetBar() {
+		fail("Not yet implemented");
+	}
+
+}
