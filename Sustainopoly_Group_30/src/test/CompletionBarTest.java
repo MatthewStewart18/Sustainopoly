905d7a1549df81b6bdbd43efb74a43c3556aec15
40296786 <jyoung43@qub.ac.uk>
2023-03-24 01:20:19 +0000

    Replace CompletionBarTest.java

--- Sustainopoly_Group_30/src/test/CompletionBarTest.java
+++ Sustainopoly_Group_30/src/test/CompletionBarTest.java
@@ -1,39 +1,40 @@
 package test;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import sustainopoly.CompletionBar;
-import sustainopoly.Game;
 import junit.framework.TestCase;
 
-public class CompletionBarTest extends TestCase {
+public class CompletionBarTest {
 	
-//	protected void setUp() throws Exception {
-//		super.setUp();
-//		int completionBar=5,amount=7;
-//	}
+	@BeforeEach
+	protected void setUp() throws Exception {
+		super.setUp();
+		public static int completionBar = 0;
+		public boolean completionDone = false;
+		private int max = 15;
+		public double updateCompletionBar;
+	}
 	
+	CompletionBar Cbar;
 
 	@Test
 	public void testGetValue() {
 		double expected = 5.0;
-		double actual = CompletionBar.getValue();
+		double actual = Cbar.getValue();
 		assertEquals(actual,expected);
 		System.out.println("Test for getValue() completed");
 	}
 
 	@Test
-	public void testSetValue() {
-		fail("Not yet implemented");
-	}
-
-	@Test
 	public void testProgress() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testGetBar() {
-		fail("Not yet implemented");
+		int expected = 20;
+		Cbar.progress(expected);
+		assertEquals(expected,Cbar.getValue());
+		assertEquals(Cbar.completionDone,true);
 	}
 
 }
