b9bd4af45e82602a60d7a54e49c0257eb6a246b7
40296786 <jyoung43@qub.ac.uk>
2023-03-24 02:59:13 +0000

    Replace CompletionBarTest.java

--- Sustainopoly_Group_30/src/test/CompletionBarTest.java
+++ Sustainopoly_Group_30/src/test/CompletionBarTest.java
@@ -9,32 +9,35 @@ import sustainopoly.CompletionBar;
 import junit.framework.TestCase;
 
 public class CompletionBarTest {
+	CompletionBar cbar = new CompletionBar(15);
 	
 	@BeforeEach
 	protected void setUp() throws Exception {
-		super.setUp();
-		public static int completionBar = 0;
-		public boolean completionDone = false;
-		private int max = 15;
-		public double updateCompletionBar;
+		CompletionBar.completionBar = 0;
+		cbar.completionDone = false;
 	}
-	
-	CompletionBar Cbar;
 
 	@Test
 	public void testGetValue() {
-		double expected = 5.0;
-		double actual = Cbar.getValue();
+		double expected = 0;
+		double actual = CompletionBar.getValue();
 		assertEquals(actual,expected);
 		System.out.println("Test for getValue() completed");
+	}
+	
+	@Test
+	public void testIsCompleted() {
+		boolean expected = false;
+		boolean actual = cbar.isCompleted();
+		assertEquals(actual,expected);
 	}
 
 	@Test
 	public void testProgress() {
 		int expected = 20;
-		Cbar.progress(expected);
-		assertEquals(expected,Cbar.getValue());
-		assertEquals(Cbar.completionDone,true);
+		cbar.progress(expected);
+		assertEquals(expected,CompletionBar.getValue());
+		assertEquals(cbar.completionDone,true);
 	}
 
 }
