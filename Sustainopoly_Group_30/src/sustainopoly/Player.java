9dc585d99391e3474ea2833e6e5d48dd8b855b4a
40296786 <jyoung43@qub.ac.uk>
2023-03-24 01:21:54 +0000

    Replace Player.java

--- Sustainopoly_Group_30/src/sustainopoly/Player.java
+++ Sustainopoly_Group_30/src/sustainopoly/Player.java
@@ -5,36 +5,34 @@ import java.util.ArrayList;
 public class Player {
 
 	private ArrayList<Investment> investments = new ArrayList<Investment>();
-	private final String PLAYER_NAME;
+	private final String playerName;
 	private int position;
 	private int playerTime = 40;
 	private int playerMoney = 1500;
-	private final int PLAYER_ID;
+	private final int playerID;
 
 	/**
 	 * constructor
-	 * 
 	 * @param name
 	 * @param playerID
 	 */
 	public Player(String name, int playerID) {
-		this.PLAYER_NAME = name;
-		this.PLAYER_ID = playerID;
+		this.playerName = name;
+		this.playerID = playerID;
 		position = 0;
 	}
 
+
 	/**
 	 * method to return playerName
-	 * 
 	 * @return
 	 */
 	public String getName() {
-		return PLAYER_NAME;
+		return playerName;
 	}
-
+	
 	/**
 	 * method to return position
-	 * 
 	 * @return
 	 */
 	public int getPosition() {
@@ -43,7 +41,6 @@ public class Player {
 
 	/**
 	 * method to return money
-	 * 
 	 * @return
 	 */
 	public int getMoney() {
@@ -52,7 +49,6 @@ public class Player {
 
 	/**
 	 * method to return time
-	 * 
 	 * @return
 	 */
 	public int getTime() {
@@ -61,29 +57,46 @@ public class Player {
 
 	/**
 	 * method to add money to a player's account
-	 * 
 	 * @param addMoney
 	 */
 	public void addMoney(int addMoney) {
-		
+		if (playerMoney < -addMoney) {
+			noFunds(-addMoney - playerMoney);
+		}
 		this.playerMoney += addMoney;
 	}
-
+	
 	/**
 	 * method to add time to a player's account
-	 * 
 	 * @param addTime
 	 */
 	public void addTime(int addTime) {
-	
+		if (playerMoney < -addTime) {
+			noFunds(-addTime - playerMoney);
+		}
 		this.playerTime += addTime;
 	}
+	
+	/**
+	 * method to print out 
+	 * @param amountNeeded
+	 */
+	private void noFunds(int amountNeeded) {
+		System.out.println("you are short $" + amountNeeded);
+	}
 
-
+	/**
+	 * method to pay
+	 * @param receiving
+	 * @param amount
+	 */
+	public void pay(Player receiving, int amount) {
+		receiving.addMoney(amount);
+		addMoney(-amount);
+	}
 
 	/**
 	 * method to move player
-	 * 
 	 * @param numSquares
 	 * @param board
 	 */
@@ -92,57 +105,48 @@ public class Player {
 
 		// passing go
 		if (position >= 20) {
-
-			playerMoney -= 50;
+			
+			playerMoney -= 100;
 			playerTime = 40;
 			position %= 20;
 			board.setTime(playerTime);
 			board.setMoney(playerMoney);
 		}
 
-		board.movePlayer(this.PLAYER_ID, position);
+		board.movePlayer(this.playerID, position);
 	}
 
 	/**
 	 * method to calculate where to move player and to call move
-	 * 
 	 * @param toPosition
 	 * @param board
 	 */
 	public void moveTo(int toPosition, Board board) {
-		move((20 - position + toPosition) % 20, board);
+		move((25 - position + toPosition) % 25, board);
 	}
 
 	/**
 	 * method to invest
-	 * 
 	 * @param investment
 	 */
 	// adding invest to players investments
-	public void investIn(int money, int time, String squareName) {
-		boolean exists = false;
-		for (int i = 0; i < investments.size(); i++) {
-
-			if (investments.get(i).getName().equals(squareName)) {
-				investments.get(i).addMoney(money);
-				investments.get(i).addTime(time);
-				exists = true;
-			}
-		}
-		if (exists == false) {
-			investments.add(new Investment(squareName, money, time));
-		}
+	public void investIn(Investment investment) {
+		addMoney(-investment.getPrice());
+		investments.add(investment);
 
 	}
 
-	public ArrayList<Investment> listInvestments() {
-
+	public  ArrayList<Investment> listInvestments() {
+		
 		return investments;
-
+		
 	}
 
-	public int getPlayerID() {
-		return PLAYER_ID;
-	}
+	  /**
+	   * returns playerID
+	   * @return
+	   */
+	  public int getPlayerID() { return playerID; }
+	 
 
 }
